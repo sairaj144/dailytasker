c044d4c2d07c11ff17b1e835f67ecdfa0f11aa2b
sairaj <sairajgadi25@gmail.com>
2024-07-04 17:31:01 +0000

    enhanced-todo

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -39,10 +39,71 @@ const initialTodosList = [
   },
 ]
 
+const iniList = initialTodosList.map(each => {
+  const newarr = {...each, isedit: false}
+  return newarr
+})
 // Write your code here
 class SimpleTodos extends Component {
   state = {
-    usersDetailsList: initialTodosList,
+    usersDetailsList: iniList,
+    newval: '',
+    newid: 9,
+    isedit: false,
+  }
+
+  handleChange = ent => {
+    this.setState({newval: ent.target.value})
+  }
+
+  handleAddTodo = () => {
+    const {usersDetailsList, newval, newid} = this.state
+    let n = newval.slice(-1)
+    const repVal = newval.slice(0, -1)
+    if (Number(n)) {
+      n = parseInt(n)
+      let i = 0
+      let newlist = usersDetailsList
+      let repeatId = newid
+      while (i < n) {
+        newlist = [...newlist, {id: repeatId, title: repVal, isedit: false}]
+        repeatId += 1
+        i += 1
+      }
+      this.setState({
+        usersDetailsList: newlist,
+        newval: '',
+        newid: newlist.length + 1,
+      })
+    } else {
+      const newlist = [
+        ...usersDetailsList,
+        {id: newid, title: newval, isedit: false},
+      ]
+      this.setState({
+        usersDetailsList: newlist,
+        newval: '',
+        newid: newlist.length + 1,
+      })
+    }
+  }
+
+  changename = id => {
+    const {usersDetailsList} = this.state
+    const updatedTodoList = usersDetailsList.map(todo =>
+      todo.id === id ? {...todo, isedit: !todo.isedit} : todo,
+    )
+
+    this.setState({usersDetailsList: updatedTodoList})
+  }
+
+  editTitle = (id, updtitle) => {
+    const {usersDetailsList} = this.state
+    const updatedTodoList = usersDetailsList.map(todo =>
+      todo.id === id ? {...todo, title: updtitle} : todo,
+    )
+
+    this.setState({usersDetailsList: updatedTodoList})
   }
 
   deleteUser = id => {
@@ -54,18 +115,35 @@ class SimpleTodos extends Component {
   }
 
   render() {
-    const {usersDetailsList} = this.state
+    const {usersDetailsList, newval} = this.state
 
     return (
       <div className="app-container">
         <div className="card">
           <h1 className="title">Simple Todos</h1>
+          <div className="insertcard">
+            <input
+              type="text"
+              value={newval}
+              onChange={this.handleChange}
+              placeholder="Enter todo title"
+            />
+            <button
+              className="addbtn"
+              onClick={this.handleAddTodo}
+              type="button"
+            >
+              Add
+            </button>
+          </div>
           <ul className="list-container">
             {usersDetailsList.map(eachUser => (
               <UserProfile
                 userDetails={eachUser}
                 key={eachUser.id}
                 deleteUser={this.deleteUser}
+                changename={this.changename}
+                editTitle={this.editTitle}
               />
             ))}
           </ul>
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,17 +1,41 @@
 import './index.css'
 
 const UserProfile = props => {
-  const {userDetails, deleteUser} = props
-  const {title, id} = userDetails
+  const {userDetails, deleteUser, changename, editTitle} = props
+  const {title, id, isedit} = userDetails
+  let tit = title
+
   const onDelete = () => {
     deleteUser(id)
   }
+
+  const changebtn = () => {
+    changename(id)
+  }
+
+  const handleChange = event => {
+    tit = event.target.value
+    editTitle(id, tit)
+  }
+
+  const newbtn = isedit ? 'Save' : 'Edit'
+
   return (
     <li className="todoitem">
-      <p className="todo">{title}</p>
-      <button className="delete" onClick={onDelete} type="button">
-        Delete
-      </button>
+      {isedit ? (
+        <input type="text" value={tit} onChange={handleChange} />
+      ) : (
+        <p className="todo">{title}</p>
+      )}
+
+      <div className="btnblock">
+        <button className="delete" onClick={changebtn} type="button">
+          {newbtn}
+        </button>
+        <button className="delete" onClick={onDelete} type="button">
+          Delete
+        </button>
+      </div>
     </li>
   )
 }
